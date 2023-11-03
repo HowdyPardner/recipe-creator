bfe803beb8860d0e3e594d4c710d60c1717de247
Marco Bragado <msb.misc@gmail.com>
2023-11-03 09:57:47 -0700

    Implement IngredientsForm component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import IngredientsForm from './components/RecipeForm';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  /* 
+    title: { type: String, required: true, },
+    description: { type: String, },
+    ingredients: [{ name: String, quantity: String, unit: String }],
+    instructions: [{ type: String }],
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // 'User' will be a reference to the User model
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date },
+    tags: [{ type: String }],
+    imageURL: { type: String, },
+  */
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        Hello from Main!
+      <IngredientsForm />
+      
     </>
   )
 }
